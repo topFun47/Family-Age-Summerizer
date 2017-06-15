4c97f9633363fea44be911a35208299fc16fc354
topFun47 <pavel.pstepanov@gmail.com>
2017-06-15 00:21:37 -0400

    Family Age Summerizer

---
+++ FamilyAgeSummerizer.rb
@@ -0,0 +1,10 @@
+#
+# Family Age Summerizer
+# Programmed By: Paul Stepanov
+#
+puts " This is a simple program that adds up all the ages of your family"
+puts " Use [Y/N] to answer any questions "
+print "Do you have any parents? "
+answer = gets
+answer = answer.chomp
+print 
